b8c51ff95a031c11e983297e6bc84418b72a9390
czer <czerpdx@gmail.com>
2023-10-24 17:28:56 -0700

    attempted to use nstask to get netstat output but action is disallowed. possibly due to sandboxed environment. may not be possible to get this information through this method but need to do more research.

--- SystemInfoApp/SystemInfoApp/NetworkViewController.m
+++ SystemInfoApp/SystemInfoApp/NetworkViewController.m
@@ -18,7 +18,28 @@
 
 // After the view has loaded this will run to set up and initialize the controller.
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
+  [NetworkViewController testNetStatNSTask];
+}
+
++ (void)testNetStatNSTask {
+  // Create the task for netstat
+  NSTask *task = [[NSTask alloc] init];
+  [task setLaunchPath:@"/usr/sbin/netstat"];
+
+  // Create a pipe to read the information back to the program
+  NSPipe *pipe = [NSPipe pipe];
+  [task setStandardOutput:pipe];
+
+  // Try running the task
+  NSError *error;
+  [task launchAndReturnError:&error];
+  // Should check error here
+  
+  NSFileHandle *fileHandleForReading = [pipe fileHandleForReading];
+  
+  NSData *dataFromFileHandle = [fileHandleForReading readDataToEndOfFileAndReturnError:&error];
+
 }
 
 
